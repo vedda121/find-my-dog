e8e98e35c7761ebea6bb2fd338ba726e4eb0fa92
Vedant <vmahes4@uic.edu>
2023-06-02 01:45:15 -0500

    Created login component UI

---
+++ src/components/login.tsx
@@ -0,0 +1,46 @@
+const Login = () => {
+  return (
+    <div className="flex justify-center items-center mx-auto md:h-screen">
+      <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
+        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+          <form className="space-y-4 md:space-y-6" action="#">
+            <div>
+              <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 ">
+                Your email
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5 "
+                placeholder="xyz@xyz.com"
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">
+                Password
+              </label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="••••••••"
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
+                required
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+            >
+              Sign in
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
